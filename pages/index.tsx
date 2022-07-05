4ffcc0f7790e30ea72f2ef48e639433aceeae9a6
Luis Valgoi <luisvalgoi@gmail.com>
2022-07-05 13:22:57 -0300

    feat: clickable social medias

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,15 @@ import { Box, Flex, Heading, Stack, Text } from '@chakra-ui/react';
 
 import { ProfileImageArea, ProfileImageBorder, profileImageStyle, socialMediaIcons } from '../styles';
 
-const URL_ORCAMENTO = 'https://my.forms.app/arqbrunaferri/orcamento';
-const URL_RENDER =
+const URL_SOCIAL_MEDIA_FB = 'https://www.facebook.com/arqbrunaferri';
+const URL_SOCIAL_MEDIA_INSTA = 'https://www.instagram.com/arqbrunaferri';
+const URL_SOCIAL_MEDIA_PIN = 'https://br.pinterest.com/arqbrunaferri';
+
+const URL_CTA_ORCAMENTO = 'https://my.forms.app/arqbrunaferri/orcamento';
+const URL_CTA_RENDER =
   'https://api.whatsapp.com/send?phone=5551981909312&text=Ol%C3%A1,%20como%20funciona%20o%20seu%20servi%C3%A7o%20de%20render?%20';
-const URL_CONTATO = 'mailto:arqbrunaferri@gmail.com';
-const URL_TCC = 'https://drive.google.com/file/d/1_GfCgB1u4dnGJR0bN5qHZg04TluvWq3B/view?usp=sharing';
+const URL_CTA_CONTATO = 'mailto:arqbrunaferri@gmail.com';
+const URL_CTA_TCC = 'https://drive.google.com/file/d/1_GfCgB1u4dnGJR0bN5qHZg04TluvWq3B/view?usp=sharing';
 
 const Home: NextPage = () => {
   return (
@@ -48,9 +52,9 @@ const Home: NextPage = () => {
       <br />
 
       <Flex as="article" w="full" justify="space-evenly" align="center">
-        <CgFacebook size="2.4rem" css={socialMediaIcons} />
-        <BsInstagram size="2.8rem" css={socialMediaIcons} />
-        <FaPinterestP size="2.4rem" css={socialMediaIcons} />
+        <CgFacebook size="2.4rem" css={socialMediaIcons} onClick={() => window.open(URL_SOCIAL_MEDIA_FB, '_blank')} />
+        <BsInstagram size="2.8rem" css={socialMediaIcons} onClick={() => window.open(URL_SOCIAL_MEDIA_INSTA, '_blank')} />
+        <FaPinterestP size="2.4rem" css={socialMediaIcons} onClick={() => window.open(URL_SOCIAL_MEDIA_PIN, '_blank')} />
       </Flex>
 
       <Flex w="full" as="article" justify="center" align="center" marginTop="4">
@@ -63,16 +67,16 @@ const Home: NextPage = () => {
 
       <Stack as="article" direction="column" spacing="12px" justify="center" align="center" mt="4" w="full">
         <Box w="full" justifyContent="center">
-          <ButtonCTA link={URL_ORCAMENTO!}>SOLICITAR ORÇAMENTO</ButtonCTA>
+          <ButtonCTA link={URL_CTA_ORCAMENTO!}>SOLICITAR ORÇAMENTO</ButtonCTA>
         </Box>
         <Box w="full" justifyContent="center">
-          <ButtonCTA link={URL_RENDER!}>SOLICITAR RENDER</ButtonCTA>
+          <ButtonCTA link={URL_CTA_RENDER!}>SOLICITAR RENDER</ButtonCTA>
         </Box>
         <Box w="full" justifyContent="center">
-          <ButtonCTA link={URL_CONTATO!}>ENTRAR EM CONTATO</ButtonCTA>
+          <ButtonCTA link={URL_CTA_CONTATO!}>ENTRAR EM CONTATO</ButtonCTA>
         </Box>
         <Box w="full" justifyContent="center">
-          <ButtonCTA link={URL_TCC!}>MAIS SOBRE MEU TCC</ButtonCTA>
+          <ButtonCTA link={URL_CTA_TCC!}>MAIS SOBRE MEU TCC</ButtonCTA>
         </Box>
       </Stack>
 
