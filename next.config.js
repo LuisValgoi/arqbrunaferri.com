7c8e2bbd9f1283098c4bac9396de32f76330ce1a
Luis Valgoi <luisvalgoi@gmail.com>
2022-07-05 13:59:22 -0300

    chore: build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    };
+  },
 };
-
-module.exports = nextConfig;
