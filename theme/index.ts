a8dab9cc166763b3fd7419ef443b0e2c67f670f6
Luis Valgoi <luisvalgoi@gmail.com>
2022-07-05 11:33:48 -0300

    feat: added more theme configs

--- theme/index.ts
+++ theme/index.ts
@@ -1,19 +1,30 @@
-import { extendTheme, Theme } from '@chakra-ui/react';
+import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 
-const theme = extendTheme({
-  fonts: {
-    heading: `'Emperatriz', sans-serif`,
-    body: `'Fira Sans', sans-serif`,
-  },
-  colors: {
-    arqbrown: {
-      200: "#9f6037",
-      300: "#744c2f",
-      500: "#523521",
-      700: "#29261d",
-      800: "#332013",
+const theme = extendTheme(
+  withDefaultColorScheme({
+    colorScheme: 'arqbrown',
+    components: ['Button'],
+  }),
+  {
+    fonts: {
+      heading: `'Emperatriz', sans-serif`,
+      body: `'Fira Sans', sans-serif`,
+    },
+    colors: {
+      arqbrown: {
+        50: '#E7D3C5',
+        100: '#D0A88B',
+        200: '#BE8760',
+        300: '#9F6841',
+        400: '#744c2f',
+        500: '#654229',
+        600: '#573923',
+        700: '#492F1D',
+        800: '#3A2617',
+        900: '#3A2617',
+      },
     },
   },
-});
+);
 
 export default theme;
