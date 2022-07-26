0e578d27f8d93fe9ed6c93ebd653ce1164756b27
Luis Valgoi <luisvalgoi@gmail.com>
2022-07-25 22:22:18 -0300

    chore(next.config.js): run prettier

--- next.config.js
+++ next.config.js
@@ -10,10 +10,10 @@ const nextConfig = {
           {
             key: 'Cache-Control',
             value: 'public, max-age=9999999999, must-revalidate',
-          }
+          },
         ],
       },
-    ]
+    ];
   },
   async redirects() {
     return [
